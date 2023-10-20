47a96377497fb5726c3d89b9c146a7ec2287cbff
Tako <m.bbird21@proton.me>
2023-10-20 12:38:28 +0200

    delete some files

--- test/test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-console.log('Hello World');
-// let filename = "TEST";
-// Crée un écouteur de système de fichiers qui surveille les fichiers .ts
-// Lorsqu'un fichier .cpp est créé
-// Ouvre le document nouvellement créé
-// Affiche le document dans l'éditeur
-// Insère "Hello World" au début du fichier
-//# sourceMappingURL=test.js.map
